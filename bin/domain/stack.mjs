7ca1c2de6dd293c7587e62d7b354dd0aad3d7fbd
Eric Tsiliacos <erictsiliacos@gmail.com>
2020-09-07 23:41:06 -0700

    rename variable

--- bin/domain/stack.mjs
+++ bin/domain/stack.mjs
@@ -2,9 +2,6 @@ const additional = message => stack => [message, ...stack];
 
 const initial = () => [];
 
-const whoseTopMessageIs = transform => ([head, ...rest]) => [
-  transform(head),
-  ...rest,
-];
+const whoseTopMessageIs = f => ([head, ...rest]) => [f(head), ...rest];
 
 export { additional, initial, whoseTopMessageIs };
